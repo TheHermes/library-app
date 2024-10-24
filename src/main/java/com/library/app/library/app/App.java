5246d1bf1b567b338295e92444072c501b85177b
TheHermes <hkvheik@gmail.com>
2024-10-24 11:24:45 +0300

    Test

--- src/main/java/com/library/app/library/app/App.java
+++ src/main/java/com/library/app/library/app/App.java
@@ -6,5 +6,6 @@ package com.library.app.library.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Library!");
+        System.out.print("Hello and welcome to the library!");
     }
 }
