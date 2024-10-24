1fa27c5712e17db534fa1fce480fb797dbf0a388
Victor Liljequist <liljequv@arcada.fi>
2024-10-24 18:38:13 +0300

    v3

--- src/main/java/com/library/app/library/app/Book.java
+++ src/main/java/com/library/app/library/app/Book.java
@@ -1,33 +1,36 @@
 package com.library.app.library.app;
 
 public class Book {
-	
-	String title;
-	String author;
-	String isbn;
-	
-	public Book(String title, String author, String isbn) {
-		this.title = title;
-		this.author = author;
-		this.isbn = isbn;
-	}
-	
-	public String getTitle() {
-		return title;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public String getIsbn() {
-		return isbn;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean available;
+
+    public Book(String title, String author, String isbn, boolean available) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.available = available;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
+
