048bae1ebd7aaaf2d4052a9740a3b557d9a43ad1
TheHermes <hkvheik@gmail.com>
2024-10-24 11:28:14 +0300

    Edit

--- src/main/java/com/library/app/library/app/App.java
+++ src/main/java/com/library/app/library/app/App.java
@@ -5,7 +5,7 @@ package com.library.app.library.app;
  */
 public class App {
     public static void main(String[] args) {
-    	System.out.print("Version 1 of library!");
+    	System.out.print("Version 1.0 of library!");
         System.out.println("Welcome to the Library!");
     }
 }
