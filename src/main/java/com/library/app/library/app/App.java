1693c405deec8883a2ce0e03f736e68347faf772
TheHermes <hkvheik@gmail.com>
2024-10-24 11:30:55 +0300

    V2 testing

--- src/main/java/com/library/app/library/app/App.java
+++ src/main/java/com/library/app/library/app/App.java
@@ -6,6 +6,7 @@ package com.library.app.library.app;
 public class App {
     public static void main(String[] args) {
     	System.out.print("Welcome to Version 2.0 of the Library");
+    	System.out.print("Hello again!");
         System.out.println("Welcome to the Library!");
     }
 }
